648e83aa9a15d99176fb490a6f67d4ded13d2a41
Andrew Tunggal <artunggal10@berkeley.edu>
2022-02-11 22:44:50 -0800

    changes to update script for pipeline

--- fe_heroes_scrape.py
+++ fe_heroes_scrape.py
@@ -1,142 +1,381 @@
 import urllib.request
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
 import csv
 import pandas as pd
 import numpy as np
+from scipy import stats
+import time
+import re
 
-# specify the url
-quote_page = "https://fireemblem.gamepress.gg/heroes"
+ser = Service("./chromedriver")
+op = webdriver.ChromeOptions()
+browser = webdriver.Chrome(service=ser, options=op)
 
-class AppURLopener(urllib.request.FancyURLopener):
-    version = "Mozilla/5.0"
+gg = "https://gamepress.gg/feheroes/heroes"
+browser.get(gg)
+respData = browser.page_source
+browser.close()
+nsoup = BeautifulSoup(respData, 'html.parser')
+n_table = nsoup.find("table", id = "heroes-new-list")
 
-opener = Request("https://feheroes.gamepedia.com/Stats_Table",
-                headers = {"User-Agent": "Mozilla/5.0"})
-
-# query the website and return the html to the variable 'webpage'
-
-webpage = urlopen(opener).read()
-
-# parse the html using beautiful soup and store in variable 'soup'
-soup = BeautifulSoup(webpage, 'html.parser')
-
-
-# Take out the <div> of name and get its value
-right_table=soup.find('table', id = "max-stats-table")
-length = len(right_table.findAll("tr"))
-
-# variable placeholders for respective column info for data frame
-name = []
-color = []
-weapon_type = []
-mov_type = []
+gpress = []
 hp = []
-atk = []
-spd = []
+attack = []
+speed = []
 defense = []
 res = []
 total = []
+color = []
+weap = []
+mov = []
+rar = []
+urls = []
+other = []
 
-for row in right_table.findAll("tr"):
-    val = []
-    val = row["data-weapon-type"].split()
-    color.append(val[0])
-    weapon_type.append(val[1])
-    mov_type.append(row["data-move-type"])
-    cells = row.findAll('td')
-    name.append(cells[0].findAll('a')[0]["title"])
-    hp.append(cells[4].find(text=True))
-    atk.append(cells[5].find(text=True))
-    spd.append(cells[6].find(text=True))
-    defense.append(cells[7].find(text=True))
-    res.append(cells[8].find(text=True))
-    total.append(cells[9].find(text=True))
 
-# creating the data frame using pandas
-df = pd.DataFrame(name,columns=['Name'])
-df["Color"] = color
-df['Weapon Type'] = weapon_type
-df['Movement Type'] = mov_type
-df['HP'] = hp
-df['Attack'] = atk
-df['Speed'] = spd
-df['Defense'] = defense
-df['Resistance'] = res
-df['Total'] = total
-
-################################
-
-#grab universe from link
+for group in n_table.findAll("tr")[1::3]:
+    gpress.append(group.attrs.get("data-name"))
+    hp.append(group.attrs.get("data-hp"))
+    attack.append(group.attrs.get("data-atk"))
+    speed.append(group.attrs.get("data-spd"))
+    defense.append(group.attrs.get("data-def"))
+    res.append(group.attrs.get("data-res"))
+    total.append(group.attrs.get("data-total"))
+    color.append(group.attrs.get("data-element").split()[0])
+    weap.append(group.attrs.get("data-element").split()[-1])
+    # data-cat-1="331" - infantry
+    # data-cat-1="306" - cavalier
+    # data-cat-1="326" - armor
+    # data-cat-1="316" - flying
+    mov.append(group.attrs.get("data-cat-1"))
+    rar.append(group.attrs.get("data-stars"))
     
-opener2 = Request("https://feheroes.gamepedia.com/Hero_list",
-        headers = {"User-Agent": "Mozilla/5.0"})
+    link = group.findAll("td")[0].find("a")["href"]
+    url = "https://gamepress.gg" + link
+    urls.append(url)
+    other.append(group.attrs.get("data-cat-"))
 
-# query the website and return the html to the variable 'webpage'
+dfn = pd.DataFrame(gpress, columns = ["Name"])
+dfn["Color"] = color
+dfn["Weapon"] = weap
+dfn["MoveCode"] = mov
+dfn["Rarities"] = rar
+dfn["HP"] = hp
+dfn["Atk"] = attack
+dfn["Spd"] = speed
+dfn["Def"] = defense
+dfn["Res"] = res
+dfn["Total"] = total
 
-universe = urlopen(opener2).read()
+# for aether raids tier list - get href and tier 
+ser = Service("./chromedriver")
+op = webdriver.ChromeOptions()
+# below two lines allow us to initiate "Headless Mode", which means that selenium works silently
+op.add_argument('--headless')
+op.add_argument('--disable-gpu')
+browser = webdriver.Chrome(service=ser, options=op)
+tlist = ("https://gamepress.gg/feheroes/aether-raids-tier-list")
+browser.get(tlist)
+tData = browser.page_source
+browser.close()
+tsoup = BeautifulSoup(tData, 'html.parser')
 
-# parse the html using beautiful soup and store in variable 'soup'
-uni = BeautifulSoup(universe, 'html.parser')
+# create a dictionary containing characters and their tiers
+char_dict = {}
+i = 0
+for field in tsoup.find_all('div', attrs={'class':'field__item'}):
+    if field.find('div', id=lambda x: x and x.startswith('tier-')):
+        i += 1
+        for in_tier in field.find_all('div', attrs = {'class': 'tier-list-cell-row'}):
+            unit = in_tier.attrs.get("data-title")
+            char_dict[unit] = int(i)
 
-left_table=uni.find('table')
-length = len(left_table.findAll("tr"))
+# turns the dictionary into a df column, and adds it to our existing df
+tiers = pd.DataFrame.from_dict(char_dict, orient = "index", columns = ["Tier"])
+dfn = pd.merge(dfn, tiers, left_on = "Name", right_index = True, how = "left")
 
-# variable placeholders for respective column info for data frame
-nombre = []
+## this part of the script grabs page source data for each individual page
+ser = Service("./chromedriver")
+op = webdriver.ChromeOptions()
+# below two lines allow us to initiate "Headless Mode", which means that selenium works silently
+op.add_argument('--headless')
+op.add_argument('--disable-gpu')
+browser = webdriver.Chrome(service=ser, options=op)
+
+all_char = []
+
+for needed in urls:
+
+    browser.get(needed)
+    chardata = browser.page_source
+    
+    # has all of the html data
+    soupy = BeautifulSoup(chardata, 'html.parser')
+    all_char.append(soupy)
+        
+    # buffer for web driver
+    time.sleep(4)
+    
+    #group.findAll("td")[12] is total
+browser.quit()
+
+##################
+
+title = []
+w_upgrades = []
+personal = []
 origin = []
-col = []
-weap_type = []
-move_type = []
-rarities = []
-st_tt = []
-release = []
-lgd = []
+img = []
+is_legend = []
+move_t = []
+is_duo = []
+col2 = []
+weap2 = []
+# below are new additions
+tier = []
+# banner can be a list to easily get banner count
+banner = []
+num_banner = []
 
-tab_vals = left_table.findAll("tr")
-
-for ind in range(1, len(tab_vals)):
-    val = []
-    val = tab_vals[ind]["data-weapon-type"].split()
-    cells = tab_vals[ind].findAll("td")
+for char in all_char:
     
-    nombre.append(cells[1].findAll('a')[0]["title"])
-    origin.append(cells[2].find(text=True))
-    rare = (cells[5].findAll(text=True))
-    add = []
-    legend = False
-    for i in np.arange(len(rare)):
-        app = "None"
-        if str(rare[i]).find("–") != -1:
-            rare[i] = str(rare[i]).replace("–", "")
-        if rare[i].find(" ") != -1:
-            rare[i] = str(rare[i]).replace(" ", "")
-        if rare[i] == "TempestTrials":
-            app = "Tempest Trials"
-        if rare[i] == "GrandHeroBattle":
-            app = "Grand Hero Battle"
-        if rare[i] == "Story":
-            app = "Story"
-        if i + 1 == len(rare):
-            st_tt.append(app)
-        if rare[i] in ("3", "4", "5"):
-            add.append(int(rare[i]))
-        if rare[i] == "Legendary":
-            legend = True
-    lgd.append(legend)
-    rarities.append(add)
-    release.append(cells[6].find(text=True))
+    # get hero title
+    char_title = ""
+    for span in char.find("table", id = "hero-details-table").findAll("span"):
+        char_title += span.text
+    title.append(char_title)
     
+    weapinfo = char.find("div", id = "weapon-skills")
     
-# creating the data frame using pandas
-df2 = pd.DataFrame(nombre,columns=['Name'])
-df2["Origin"] = origin
-df2['Rarities'] = rarities
-df2['Story/TT/GHB?'] = st_tt
-df2['Release Date'] = release
-df2["Legendary"] = lgd
+    # retrieves whether the character has weapon-refines
+    if weapinfo.findAll("div", {"id": "weapon-upgrades-section"})[0].find(
+        "div", {"class": "view-content"}) is not None:
+        w_upgrades.append(True)
+    else:
+        w_upgrades.append(False)
 
-# combining the two dataframes
-heroes = pd.merge(df2, df, on = "Name")
+    # retrieves whether the character has a personal, non-inheritable weapon
+    i = 0
+    per = False
+    for item in weapinfo.findAll("div", {"class": "views-element-container"
+        })[0].findAll("tr"):
+        if i == 0:
+            i += 1
+            continue
+        if not item.findAll("div"):
+            continue
+        if "Non-Inheritable skill" in item.findAll("div")[-1].get_text():
+            per = True
+            break
+    if per is False:
+        personal.append(False)
+    else:
+        personal.append(True)
 
-heroes
+    # get origin information
+    # need to start taking into account duos with origin of multiple games
+    try:
+        attempt_orig = char.find("div", {"class": 
+         "field field--name-field-origin field--type-entity-reference field--label-hidden field__items"})
+        games = attempt_orig.findAll("div", {"class": 'field__item'})
+        if len(games) == 1:
+            origin.append(games[0].text)
+        else:
+            game_lst = []
+            for game in attempt_orig.findAll("div", {"class": 'field__item'}):
+                game_lst.append(game.text)
+            origin.append(game_lst)
+    except AttributeError:
+        origin.append("None")
+        
+    # get hero image link
+    image = char.find("div", id = "hero-image").find("img")["src"]
+    img.append("https://gamepress.gg" + image)
+
+    att = char.find("div", id = "hero-atts")
+    
+    # get information about whether character is legendary/mythic
+    is_legend.append(att.find("a", {"class": 
+        "tipso-legendary"}) is not None)
+    
+    # get move type information
+    move = att.find("div", {"class": 
+        "field field--name-field-movement field--type-entity-reference field--label-hidden field__item"})
+    move_t.append(move.get_text().replace("\n", "").replace(" ", ""))
+    
+    # get information about whether character is a duo character
+    is_duo.append(char.find("div", {"class": "duo-skill-effect"}) is not None)
+    
+    # sanity check for color/weapon
+    w_use = att.find("div", {"class": 
+        "field field--name-field-attribute field--type-entity-reference field--label-hidden field__item"})
+    colour, weapon = w_use.get_text().replace("\n", "").split()
+    col2.append(colour)
+    weap2.append(weapon)
+    
+    # get information regarding banners
+    banner_h3 = char.find("h3", text="Banners Featured In")
+    char_banner = []
+    for row in banner_h3.find_next_siblings("div")[0].findAll('td'):
+        char_banner.append(row.find("a").text)
+    banner.append(char_banner)
+    num_banner.append(len(char_banner))
+
+# take all of the data obtained and creates new columns
+dfn["Title"] = title
+dfn["Refines"] = w_upgrades
+dfn["Personal Weapon"] = personal
+dfn["Legendary/Mythic"] = is_legend
+dfn["Origin"] = origin
+dfn["Duo"] = is_duo
+dfn["Image"] = img
+dfn["Movement"] = move_t
+dfn["Banners"] = banner
+dfn["Number of Banners"] = num_banner
+
+# grabs data related to refreshers (dancers/singers)
+refresh = []
+dsurls = ["https://gamepress.gg/feheroes/command-skills/dance",
+         "https://gamepress.gg/feheroes/command-skills/sing"]
+
+ser = Service("./chromedriver")
+op = webdriver.ChromeOptions()
+# below two lines allow us to initiate "Headless Mode", which means that selenium works silently
+op.add_argument('--headless')
+op.add_argument('--disable-gpu')
+brow = webdriver.Chrome(service=ser, options=op)
+for lin in dsurls:
+    brow.get(lin)
+    ddata = brow.page_source
+    dsoup = BeautifulSoup(ddata, "html.parser")
+    since = dsoup.find("div", id = "block-gamepressbase-content")
+    char = since.find("div", {"class": "views-element-container"}).findAll("a")
+    for it in char[1::2]:
+        refresh.append(it.get_text())
+brow.quit()
+
+# creates a column with a binary indicator of whether a unit is a refresher
+ref = dfn["Name"].apply(lambda x: x in refresh)
+dfn2 = dfn.copy()
+dfn2["Refresher"] = ref
+
+# goes through the 'Rarities' column to create two features: one for how the
+# character is obtained in the game, and one for actual rarity
+
+rar = []
+ob = []
+for row in dfn2["Rarities"]:
+    st = []
+    a = row.split("_")[0]
+    if "Story" in row:
+        ob.append("Story")
+    elif "Grand_Hero_Battle" in row:
+        ob.append("GHB")
+    elif "Tempest_Trials" in row:
+        ob.append("TT")
+    elif "Enemy_Only" in row:
+        ob.append("Enemy-Only")
+    elif "Legacy" in row:
+        ob.append("Legacy")
+    else:
+        ob.append("NA")
+    for i in a.split("-"):
+        if "2" in i or "3" in i or "4" in i or "5" in i:
+            st.append(i)
+    rar.append(st)
+
+dfn2["Stars"] = rar
+dfn2["Obtain"] = ob
+# sanity check for color, since we found something off through data exploration
+dfn2["Color"] = col2
+
+# changes instances of no tier to "None"
+dfn2["Tier"] = dfn2["Tier"].fillna("None")
+
+# to get release date of characters
+ser = Service("./chromedriver")
+op = webdriver.ChromeOptions()
+# below two lines allow us to initiate "Headless Mode", which means that selenium works silently
+op.add_argument('--headless')
+op.add_argument('--disable-gpu')
+browser = webdriver.Chrome(service=ser, options=op)
+r_list = ("https://feheroes.fandom.com/wiki/List_of_Heroes")
+browser.get(r_list)
+releaseData = browser.page_source
+browser.close()
+release_soup = BeautifulSoup(releaseData, 'html.parser')
+release_table = release_soup.find_all('tr', attrs={'class':'hero-filter-element'})
+
+# this function adjusts the title so that it matches with the GamePress data, allowing
+# for merging
+
+def title_fix(title):
+    print(title)
+    title_a, title_b = title.split(":")
+    title_a = title_a.split(" ")[-1]
+    return title_a + " :" + title_b
+
+release_data_dict = {}
+origin_data_dict = {}
+
+# based on scraping that shows differences in used titles - this dictionary fixes the following needed changes
+    # Tethys: typo ("Dancer")
+    # Canas: "Wisdom Seeker in GPedia"
+    # Hatari - no info
+    # Niles - the word "be" is capitalized in Gpedia
+    # Eliwood - Marquess Pherae in GPedia
+    # Winter Tharja - quotes around "Normal Girl" in Gpedia
+title_change_dict = {"Niles - Cruel to Be Kind": "Niles - Cruel to be Kind", 
+                     "Eliwood - Marquess Pherae": "Eliwood - Marquess of Pherae",
+                    'Tharja - "Normal Girl"': "Tharja - Normal Girl",
+                    "Canas - Wisdom Seeker": "Canas - Seeker of Wisdom"}
+
+for char in release_table:
+    title = char.findAll("td")[1].text
+    title = title.replace(":", " -")
+    title = re.sub(r'\([^)]*\)', '', title)
+    if title in title_change_dict.keys():
+        title = title_change_dict[title]
+    release_date = char.findAll("td")[7].text
+    origin = char.findAll("td")[2].text
+    release_data_dict[title] = release_date
+    origin_data_dict[title] = origin
+
+# fixes a typo in GamePress' data
+dfn2.loc[dfn2["Name"] == "Tethys", "Title"] = "Tethys - Beloved Dancer"
+
+# create a new copy of our dataframe
+dfn3 = dfn2.copy()
+
+# this function fixes additional issues with title conflicts between
+# GamePress and GamePedia
+def title_fix(title):
+    to_ignore = ["Flame Emperor", "Black Knight", "Death Knight"]
+    title_a, title_b = title.split(" -")
+    if title_a in to_ignore:
+        return title_a + " -" + title_b
+    else:
+        title_a = title_a.split(" ")[-1]
+    return title_a + " -" + title_b
+
+dfn3['Title'] = dfn3['Title'].apply(lambda x: re.sub(r'\([^)]*\) ', '', x)).apply(title_fix)
+# split Title into two by the '-'. split the first half by space
+
+release_df = pd.DataFrame.from_dict(release_data_dict, orient = "index", columns = ["Release Date"])
+dfn3 = pd.merge(dfn3, release_df, left_on = "Title", right_index = True, how = "left")
+origin_df = pd.DataFrame.from_dict(origin_data_dict, orient = "index", columns = ["Origin - GPedia"])
+dfn3 = pd.merge(dfn3, origin_df, left_on = "Title", right_index = True, how = "left")
+
+# rename our dataframe
+heroes = dfn3
+heroes[["HP", "Atk", "Spd", "Def", "Res"]] = heroes[["HP", "Atk", "Spd", "Def", "Res"]].apply(pd.to_numeric)
+
+# set last_update variable to the current month and year
+last_update = time.strftime("%m-%y")
+# creates a file for .excel and .csv formats
+heroes.to_excel("hero_data_" + last_update + ".xlsx", encoding='utf-8', index=False)
+heroes.to_csv("hero_data_" + last_update + ".csv", encoding='utf-8', index=False)
